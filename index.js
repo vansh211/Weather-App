d5628fed11cfa6c998c50e765d16492aa55a8e81
vansh211 <vanshmunjal111@gmail.com>
2025-11-27 02:26:04 +0530

    new glitch fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const grantAccessContainer = document.querySelector(".grant-location-container")
 const searchForm = document.querySelector("[data-searchForm]");
 const loadingScreen = document.querySelector(".loading-container");
 const userInfoContainer = document.querySelector(".user-info-container");
+const messageContainer = document.querySelector(".error-container");
 
 
 let oldTab = userTab;
@@ -23,12 +24,14 @@ function switchTab(newTab) {
             //kya search form wala container is invisible, if yes then make it visible
             userInfoContainer.classList.remove("active");
             grantAccessContainer.classList.remove("active");
+            messageContainer.classList.remove("active");
             searchForm.classList.add("active");
         }
         else {
             //main pehle search wale tab pr tha, ab your weather tab visible karna h 
             searchForm.classList.remove("active");
             userInfoContainer.classList.remove("active");
+            messageContainer.classList.remove("active");
             //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
             //for coordinates, if we haved saved them there.
             getfromSessionStorage();
